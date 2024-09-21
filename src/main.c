39ce4b8606ebbe1a4a3f9a6f02613cbe89e2d300
Ashton Snelgrove <ashton@snelgrove.science>
2024-09-21 15:50:46 -0600

    Compiling version of main.c

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <FreeRTOS.h>
+#include <semphr.h>
 #include <task.h>
 #include <pico/stdlib.h>
 #include <pico/multicore.h>
@@ -11,31 +12,27 @@
 #define SIDE_TASK_PRIORITY      ( tskIDLE_PRIORITY + 1UL )
 #define SIDE_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
 
-SemaphoreHandle_t semaphore = NULL;
-StaticSemaphore_t semaphore_buffer;
+SemaphoreHandle_t semaphore;
 
-struct TaskHandle_t coop_thread;
-SemaphoreHandle_t semaphore = NULL;
-StaticSemaphore_t semaphore_buffer;
 int counter;
 int on;
 
-void side_thread(void)
+void side_thread(void *params)
 {
 	while (1) {
-        xTaskDelay(100);
+        vTaskDelay(100);
         counter += counter + 1;
 		printf("hello world from %s! Count %d\n", "thread", counter);
 	}
 }
 
-void main_thread(void)
+void main_thread(void *params)
 {
 	while (1) {
         cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, on);
-        xTaskDelay(100);
+        vTaskDelay(100);
 		printf("hello world from %s! Count %d\n", "main", counter++);
-        on = !on
+        on = !on;
 	}
 }
 
@@ -45,11 +42,12 @@ int main(void)
     hard_assert(cyw43_arch_init() == PICO_OK);
     on = false;
     counter = 0;
-    xSemaphoreCreateBinaryStatic( &semaphore_buffer );
-    xTaskCreate(main_task, "MainThread",
-                MAIN_TASK_STACK_SIZE, NULL, MAIN_TASK_PRIORITY, &task);
-    xTaskCreate(main_task, "SideThread",
-                SIDE_TASK_STACK_SIZE, NULL, SIDE_TASK_PRIORITY, &task);
+    TaskHandle_t main, side;
+    semaphore = xSemaphoreCreateCounting(1, 1);
+    xTaskCreate(main_thread, "MainThread",
+                MAIN_TASK_STACK_SIZE, NULL, MAIN_TASK_PRIORITY, &main);
+    xTaskCreate(side_thread, "SideThread",
+                SIDE_TASK_STACK_SIZE, NULL, SIDE_TASK_PRIORITY, &side);
     vTaskStartScheduler();
 	return 0;
 }
